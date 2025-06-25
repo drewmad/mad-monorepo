6a5a3ea6bab87240c687d0fa89d68b4594f92c14
Drew A. Madison <32315897+drewmad@users.noreply.github.com>
2025-06-25 03:35:47 -0400

    feat(web): integrate Vercel analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/globals.css';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
