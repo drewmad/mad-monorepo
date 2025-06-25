833f8d32e17d602a5f3e12b50e7ac3c80f8327d0
Drew A. Madison <32315897+drewmad@users.noreply.github.com>
2025-06-25 01:35:33 -0400

    Update ProtectedLayoutClient.tsx

--- apps/web/app/(protected)/ProtectedLayoutClient.tsx
+++ apps/web/app/(protected)/ProtectedLayoutClient.tsx
@@ -2,7 +2,6 @@
 
 import Sidebar from '@/components/sidebar/Sidebar';
 import Header from '@/components/header/Header';
-import { useSidebar } from '@/components/sidebar/context';
 
 interface ProtectedLayoutClientProps {
   user: {
@@ -14,19 +13,13 @@ interface ProtectedLayoutClientProps {
 }
 
 export default function ProtectedLayoutClient({ user, children }: ProtectedLayoutClientProps) {
-  const { expanded } = useSidebar();
-
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <div 
-        className={`flex-1 flex flex-col transition-all duration-300 ${
-          expanded ? 'ml-64' : 'ml-20'
-        }`}
-      >
+      <div className="flex-1 flex flex-col ml-[368px]"> {/* 80px + 288px */}
         <Header user={user} />
         <main className="flex-1 p-6 md:p-8">{children}</main>
       </div>
     </div>
   );
-} 
+}
